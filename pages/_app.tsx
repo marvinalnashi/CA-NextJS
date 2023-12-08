114a5064c7b6bb1734e0527cc3db18ec82174eb0
marvinalnashi <marvinalnashi@hotmail.com>
2023-12-08 15:12:07 +0100

    Re-added coming soon page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ import '@styles/dark-mode.css'
 import '@styles/prism.css'
 import '@styles/toc.css'
 import '@styles/portfolio.css'
+import '@styles/wip.css'
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
