a7e457a82e9b54f9db2d378a04dc052dd40799c1
marvinalnashi <marvinalnashiedu@gmail.com>
2023-12-26 01:28:53 +0100

    Revert "Re-added coming soon page"
    This reverts commit 114a5064c7b6bb1734e0527cc3db18ec82174eb0.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,6 @@ import '@styles/dark-mode.css'
 import '@styles/prism.css'
 import '@styles/toc.css'
 import '@styles/portfolio.css'
-import '@styles/wip.css'
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
