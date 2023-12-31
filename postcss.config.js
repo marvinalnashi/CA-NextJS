2911fc742ca6d3f7e8fc5b9a52a66381f93a51ce
marvinalnashi <marvinalnashi@hotmail.com>
2023-12-31 19:07:31 +0100

    Fixed CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: {
     'postcss-easy-import': {},
-    'postcss-color-mod-function': {},
     'postcss-preset-env': {
       autoprefixer: {
         flexbox: 'no-2009',
