c6fd7aa7d9719012ab2c2c68b647740658960018
Marvin Al Nashi <marvinalnashi@hotmail.com>
2024-01-27 23:50:43 +0100

    Revert "Revert "Bugfixes and improvements""
    This reverts commit af663902a42b04854ac56b5d785994bd1970d629.

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,5 @@
 module.exports = {
-  // see https://github.com/iamvishnusankar/next-sitemap
-  siteUrl: 'https://next.jamify.org',
+  siteUrl: 'https://codearise.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
 }
