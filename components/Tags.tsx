9f03bde54cb275f9d29930773229fc3b7dd70bfa
Marvin Al Nashi <marvinalnashi@hotmail.com>
2024-02-03 15:48:04 +0100

    Fixed all categories page tags

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -1,13 +1,49 @@
 import {categoriesData} from "@lib/categoriesData";
 import Link from "next/link";
+import React, {useEffect, useState} from "react";
+import VanillaTilt from "vanilla-tilt";
 
-const Tags = () => {
+const categories: string[] = ['Category 1', 'Category 2', 'Category 3', 'Category 4'];
+
+const Tags: React.FC = () => {
+
+  useEffect(() => {
+    const tiltNodes = Array.from(document.querySelectorAll("#tag")) as HTMLElement[];
+
+    VanillaTilt.init(tiltNodes, {
+      max: 25,
+      speed: 400,
+      glare: true,
+      "max-glare": 1,
+    });
+
+    // Clean up on component unmount
+    return () => {
+      tiltNodes.forEach((element) => {
+        // @ts-ignore
+        element.vanillaTilt.destroy();
+      });
+    };
+  }, []);
+
   return (
     <div className="tags-container">
-      {categoriesData.map((category, index) => (
-        <Link key={index} href={category.url}>
-          <a className="tag-button" style={{ margin: '5px', padding: '10px' }}>{category.name}</a>
-        </Link>
+      <h1 className="text-align">Projects</h1>
+      {categories.map((category) => (
+        <div key={category}>
+          <h2 className="text-align">{category}</h2>
+          <div className="items-container">
+            {categoriesData.map((category, index) => (
+              <div key={category.name} className="tags-card-container">
+                  <div className="tags-content">
+                    <Link key={index} href={category.url}>
+                      <a id="tag">{category.name}</a>
+                    </Link>
+                  </div>
+              </div>
+            ))}
+          </div>
+        </div>
       ))}
     </div>
   );
