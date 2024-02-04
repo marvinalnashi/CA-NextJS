85c0d8aeb245dee71effd8e9bf2f882b8fb34687
Marvin Al Nashi <marvinalnashi@hotmail.com>
2024-02-04 03:30:12 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 const defaultOptions = withBundleAnalyzer({
-  //...(process.env.NETLIFY === 'true' && { target: 'serverless' }),
   images: {
     deviceSizes: [320, 500, 680, 1040, 2080, 2048, 3120],
     domains: [
