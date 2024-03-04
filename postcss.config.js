013f253ed2fc4377f64693a18d9c0166db3050ba
Marvin Al Nashi <marvinalnashi@hotmail.com>
2024-03-04 01:07:57 +0100

    UI template update

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,6 @@
 module.exports = {
   plugins: {
-    'tailwindcss': {},
-    'autoprefixer': {},
-    'postcss-easy-import': {},
-    'postcss-preset-env': {
-      autoprefixer: {
-        flexbox: 'no-2009',
-      },
-      stage: 3,
-      features: {
-        'custom-properties': false,
-      },
-    },
+    tailwindcss: {},
+    autoprefixer: {},
   },
 }
