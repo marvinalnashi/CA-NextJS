041872a8a724c9ed18329d2fdca8f757d12d0246
Marvin Al Nashi <marvinalnashi@hotmail.com>
2024-03-04 01:09:35 +0100

    Revert "UI template update"
    This reverts commit 013f253ed2fc4377f64693a18d9c0166db3050ba.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    'tailwindcss': {},
+    'autoprefixer': {},
+    'postcss-easy-import': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
   },
 }
--- providers/TosterProvider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import { Toaster } from "react-hot-toast";
-
-const TosterProvider = () => {
-	return <Toaster />;
-};
-
-export default TosterProvider;
