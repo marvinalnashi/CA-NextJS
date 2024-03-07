5100f9f45ab43b15022003323315f42c2debe052
Marvin Al Nashi <marvinalnashi@hotmail.com>
2024-03-07 10:26:34 +0100

    Updated blog cards margins and sizes on homepage

--- components/SaasApp/OurLatestBlog.tsx
+++ components/SaasApp/OurLatestBlog.tsx
@@ -13,20 +13,21 @@ const HomeBlog: React.FC<HomeBlogProps> = ({ posts, settings, bodyClass }) => {
   return (
     <div>
       <div className="container">
-        <div className="grid gap-[25px] grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {posts.map((post, i) => (
-          <HomePostCard key={post.id} settings={settings} post={post} num={i} aosDelay={`${100 * (i + 1)}`} />
-        ))}
-        <div className="text-center mt-[30px] md:mt-[40px]" data-aos="fade-in" data-aos-delay="100" data-aos-duration="600" data-aos-once="false">
+        <div className="grid gap-[20px] grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {posts.map((post, i) => (
+            <HomePostCard key={post.id} settings={settings} post={post} num={i} aosDelay={`${100 * (i + 1)}`}/>
+          ))}
+        </div>
+        <div className="text-center mt-[30px] md:mt-[40px]" data-aos="fade-in" data-aos-delay="100"
+             data-aos-duration="600" data-aos-once="false">
           <Link
             href="/blog"
-            className="py-[15px] px-[30px] inline-block rounded-[6px] bg-primary text-white font-semibold text-[16px] md:text-[18px] transition duration-500 ease-in-out hover:bg-black-color hover:text-white"
+            className="blog-btn-margin py-[15px] px-[30px] inline-block rounded-[6px] bg-primary text-white font-semibold text-[16px] md:text-[18px] transition duration-500 ease-in-out hover:bg-black-color hover:text-white"
           >
             See All News
           </Link>
         </div>
       </div>
-    </div>
     </div>
   )
 }
